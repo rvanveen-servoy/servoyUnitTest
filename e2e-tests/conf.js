24d5a4af785e3f0df20ccedf9311eaf36ac10d0a
Anastasija <efistasija@gmail.com>
2017-02-17 11:14:38 +0100

    added e2e ant scripts and setup

--- e2e-tests/conf.js
+++
@@ -1,8 +0,0 @@
-exports.config = {
-  seleniumAddress: 'http://localhost:4444/wd/hub',
-  onPrepare: function() {
-	browser.manage().window().setSize(1600, 1000);
-  },
-  //specs: ['custom_protractor.js']
-  specs: ['sampleFormE2ETest.js']
-};
